a5f74da8d663987632bc68279ee0b47de23b92ff
Gary <gbautista2@knights.ucf.edu>
2020-05-06 10:00:34 -0400

    added Game rspec

--- spec/Game_spec.rb
+++ spec/Game_spec.rb
@@ -3,4 +3,26 @@ require 'Game'
 
 describe Game do 
 
+    describe "#initialize" do 
+
+        it "holds a new deck" do 
+            game.deck
+        end
+
+        it "holds up to 4 players" do 
+            expect(game.players.length).to be < 5
+        end
+
+        it "has a discard pile as an attribute" do 
+            game.discard_pile
+        end
+
+        it "has a @game_pot attribute with a value of zero" do 
+            expect(game.game_pot).to eq(0)
+        end
+    end
+
+    describe "#play" do 
+        it "goes through the game logic"
+    end
 end
